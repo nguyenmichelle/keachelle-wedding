558afb13fb427369a626b4eab53c5859a618b2dc
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro-2.local>
2023-10-17 02:49:22 -0600

    Updating wording in details section

--- components/Section/Details.vue
+++ components/Section/Details.vue
@@ -3,11 +3,12 @@
     <v-row>
       <v-col cols="12" class="text-center">
         <p><b>CATHOLIC CEREMONY</b> @ 4:30 PM<br>
-          CHAPEL AT JARDIN DEL MAR<br>
+          CHAPEL AT JARDÍN DEL MAR<br>
           77563 QUINTANA ROO<br>
           CANCUN, MEXICO</p>
-        <p><b>DINNER & CELEBRATIONS</b> TO FOLLOW<br>
-          JARDIN DEL MAR<br>
+        <p><b>RECEPTION</b> TO FOLLOW<br>
+          DINNER & CELEBRATIONS
+          JARDÍN DEL MAR<br>
           CANCUN, MEXICO</p>
       </v-col>
       <v-col cols="12"><countdown></countdown>
--- components/Section/WeddingParty.vue
+++ components/Section/WeddingParty.vue
@@ -1,5 +1,5 @@
 <template>
-  <Section id="wedding-party" background-color="#858872" title="Wedding Party" white-text="true">
+  <Section id="wedding-party" background-color="#858872" title="WEDDING PARTY" white-text="true">
     <v-row>
       <v-col cols="12" class="text-center">
         Michelle and Keaton
