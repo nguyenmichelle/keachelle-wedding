41a0809ba75b9255e870fa899ca4a01d4f692e9d
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro-2.local>
2023-10-30 22:25:00 -0600

    trying to fix deployment issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  buildDir: 'dist',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
