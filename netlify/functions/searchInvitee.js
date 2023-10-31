a7389077fb2d10bf06abc6bdec9e0ceb298f5288
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro-2.local>
2023-10-30 23:08:07 -0600

    See if mysql2 works better than prisma

--- netlify/functions/searchInvitee.js
+++ netlify/functions/searchInvitee.js
@@ -1,9 +1,24 @@
 // netlify/functions/searchInvitee.js
-const { PrismaClient } = require('@prisma/client');
+// const { PrismaClient } = require('@prisma/client');
 
 exports.handler = async (event, context) => {
-  const prisma = new PrismaClient();
-  console.log('here')
+  // const prisma = new PrismaClient();
+  const mysql = require('mysql2');
+
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      // Provide SSL/TLS options here
+      // Example options (customize to match your SSL/TLS configuration):
+      rejectUnauthorized: false
+    }
+  };
+
+  const pool = mysql.createPool(dbConfig);
+  const promisePool = pool.promise();
 
   try {
     if (event.httpMethod !== 'GET') {
@@ -19,28 +34,28 @@ exports.handler = async (event, context) => {
     console.log('here3')
 
 
-    const response = await prisma.$queryRaw`select *
+    const [rows, fields] = await promisePool.query(`select *
                                             from themiche_wedding.Invitee
                                             where id in (select parent_id
                                                          from themiche_wedding.Invitee
-                                                         where (full_name = ${searchString} or last_name = ${searchString} or
-                                                                first_name = ${searchString}))
+                                                         where (full_name = '${searchString}' or last_name = '${searchString}' or
+                                                                first_name = '${searchString}'))
                                                or parent_id in (select id
                                                                 from themiche_wedding.Invitee
-                                                                where (full_name = ${searchString} or last_name = ${searchString} or
-                                                                       first_name = ${searchString}))
+                                                                where (full_name = '${searchString}' or last_name = '${searchString}' or
+                                                                       first_name = '${searchString}'))
                                                or parent_id in (select parent_id
                                                                 from themiche_wedding.Invitee
-                                                                where (full_name = ${searchString} or last_name = ${searchString} or
-                                                                       first_name = ${searchString}))
-                                               or (full_name = ${searchString} or last_name = ${searchString} or
-                                                   first_name = ${searchString})
-                                            ORDER BY CASE WHEN parent_id IS NULL THEN id ELSE parent_id END, id;`;
+                                                                where (full_name = '${searchString}' or last_name = '${searchString}' or
+                                                                       first_name = '${searchString}'))
+                                               or (full_name = '${searchString}' or last_name = '${searchString}' or
+                                                   first_name = '${searchString}')
+                                            ORDER BY CASE WHEN parent_id IS NULL THEN id ELSE parent_id END, id;`);
     console.log('here4')
 
     return {
       statusCode: 200,
-      body: JSON.stringify(response),
+      body: JSON.stringify(rows)
     };
   } catch (error) {
     return {
@@ -48,6 +63,6 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ error: error }),
     };
   } finally {
-    await prisma.$disconnect();
+    // await prisma.$disconnect();
   }
 };
