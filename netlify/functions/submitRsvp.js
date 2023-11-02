3a01a5061c2f9e6054b9556824a1a1cca521324f
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro-2.local>
2023-11-01 20:23:15 -0600

    I think I fixed the issue with submitRSVP not working

--- netlify/functions/submitRsvp.js
+++ netlify/functions/submitRsvp.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2')
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust according to your needs
+  queueLimit: 0,
+});
+const promisePool = db.promise();
 
 exports.handler = async function (event, context) {
-  const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false
-    },
-    waitForConnections: true,
-    connectionLimit: 10, // Adjust according to your needs
-    queueLimit: 0,
-  });
-  const promisePool = db.promise();
   const data = event.queryStringParameters
 
   try {
