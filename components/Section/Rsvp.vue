bf945e30cc8b042f82248965443f40396fe1142d
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro-2.local>
2023-11-06 21:27:00 -0700

    Trying to update post call to sendEmail to see if this fixes things

--- components/Section/Rsvp.vue
+++ components/Section/Rsvp.vue
@@ -217,14 +217,20 @@ export default {
     async sendEmail() {
       this.emailLoading = true
 
-      try {
-        const response = await axios.post('/.netlify/functions/sendMail', {
-            attending1: this.currentRsvpGroup.filter(person => person.attending_welcome_party == 1),
-            notAttending1: this.currentRsvpGroup.filter(person => person.attending_welcome_party == 0),
-            attending2: this.currentRsvpGroup.filter(person => person.attending_wedding == 1),
-            notAttending2: this.currentRsvpGroup.filter(person => person.attending_wedding == 0),
-            email: this.currentRsvpGroup[0].email
+      const data = JSON.stringify({
+        attending1: this.currentRsvpGroup.filter(person => person.attending_welcome_party == 1),
+        notAttending1: this.currentRsvpGroup.filter(person => person.attending_welcome_party == 0),
+        attending2: this.currentRsvpGroup.filter(person => person.attending_wedding == 1),
+        notAttending2: this.currentRsvpGroup.filter(person => person.attending_wedding == 0),
+        email: this.currentRsvpGroup[0].email
       })
+
+      try {
+        const response = await axios.post('/.netlify/functions/sendMail', data, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
         console.log("Raw Response Data:", response.data);
 
         this.emailSent = true
